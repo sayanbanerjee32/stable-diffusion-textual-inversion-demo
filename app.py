74aca1fac07a106e289ca774433b96eec70eb9f7
sayanbanerjee32 <sayanbanerjee32@gmail.com>
2024-08-09 18:59:22 +0530

    text image similarity loss

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import gradio as gr
-from sd import stl_list, img_size_opt_dict, generate_images
+from sd import (stl_list, 
+                img_size_opt_dict, 
+                loss_fn_dict,
+                generate_images)
 
 with gr.Blocks() as demo:
-    gr.HTML("<h1 align = 'center'> Stable Diffusion - Text Inversion and additional guidence</h1>")
-    gr.HTML("<h4 align = 'center'> Generates imgaes based on the prompt and 5 different styles and then with additional guidence of hue loss</h4>")
-    gr.HTML("<h6 align = 'center'> !!The image generation may take 5 to 10 minutes on CPU!!</h4>")
+    gr.HTML("<h1 align = 'center'> Stable Diffusion - Text Inversion and additional guidance</h1>")
+    gr.HTML("<h4 align = 'center'> Generates imgaes based on the prompt and 5 different styles and then with additional guidance</h4>")
+    gr.HTML("<h6 align = 'center'> !! The image generation may take 5 to 10 minutes on CPU per image !!</h4>")
 
     with gr.Row():
         content = gr.Textbox(label = "Enter prompt text here")
@@ -16,26 +19,40 @@ with gr.Blocks() as demo:
         num_steps = gr.Slider(1, 50, step = 1, value=30, label="Number of inference steps", info="Choose between 1 and 50")
         # gr.Number(value = 10, label = "Number of inference steps")
 
-    
+
     with gr.Row():
         stl_dropdown = gr.Dropdown(
-            stl_list, 
-            value=stl_list[:1], multiselect=True, label="Style", 
+            stl_list,
+            value=stl_list[:1], multiselect=True, label="Style",
             info="Styles to be applied on images"
         )
         size_dropdown = gr.Dropdown(
-            [*img_size_opt_dict], 
+            [*img_size_opt_dict],
             value = [*img_size_opt_dict][-1],
             label="Image size", info="Target size for generated images"
         )
-    
+
+    with gr.Row():
+        loss_radio = gr.Radio([*loss_fn_dict], value = [*loss_fn_dict][0],
+                              label="Additional guidance loss", info="The loss to be applied")
+        text_loss_text = gr.Textbox(label = "Enter additional guidance text here if text-image similarity loss is selected",
+                                interactive = False)
+        gr.Examples([
+            "White background",
+            "Low quality",
+            "Blurred"
+        ],
+        inputs = text_loss_text)
+
     inputs = [
                 content,
                 num_steps,
                 stl_dropdown,
-                size_dropdown
+                size_dropdown,
+                loss_radio,
+                text_loss_text
                 ]
-    
+
     generate_btn = gr.Button(value = 'Generate')
 
     with gr.Row():
@@ -51,7 +68,7 @@ with gr.Blocks() as demo:
         outputs  = [wo_add_guide, add_guide ]
     generate_btn.click(fn = generate_images, inputs= inputs, outputs = outputs)
 
-# for collab
+# # for collab
 # demo.launch(debug=True)
 
 if __name__ == '__main__':
